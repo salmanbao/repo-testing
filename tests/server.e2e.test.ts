44b04aed59bf51615261fc066426ec114c9fa117
salmanbao <salmancodez@gmail.com>
2024-05-13 21:37:17 +0400

    add 01 feature

--- tests/server.e2e.test.ts
+++ tests/server.e2e.test.ts
@@ -3,11 +3,13 @@
 describe('Server Test File: Get /', () => {
     it('should return Hello World', async () => {
         const expectedResult = 'Hello World';
+        const Result = 'Hello World';
+
 
         console.log(expectedResult);
         // const response = await request(server).get('/');
 
         // expect(response.status).toEqual(200);
-        // expect(response.text).toEqual(expectedResult);
+        expect(Result).toEqual(expectedResult);
     });
 });
